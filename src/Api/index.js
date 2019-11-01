205136ae37679230c409b9ba66051d196f659a25
Come2BtheOne <582271833@qq.com>
2019-11-01 13:15:59 +0800

    合并

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,70 +1,66 @@
 import axios from 'axios';
-<<<<<<< HEAD
-let gowhere = axios.create({
-    baseURL:'http://localhost:12345'
-})
-async function get(params, config = {}) {  
-    let { data } = await gowhere.get('/home', {
-        ...config,
-=======
 
 let gowhere = axios.create({
     baseURL: 'http://localhost:12345'
 })
+async function get(params, config = {}) {
+    let { data } = await gowhere.get('/home', {
+        ...config,
+        params
+    });
+    return data
+}
 
 
 async function list(params) {
-    let data  = await gowhere.get('/list', {
->>>>>>> 3cec31e772c2e19eda985df87c0ea1e11a7ba78e
+    let data = await gowhere.get('/list', {
         params
     });
     return data;
 }
 
-<<<<<<< HEAD
-=======
 async function checkAttention(params) {
-    let data  = await gowhere.get('/content/checkgz', {
+    let data = await gowhere.get('/content/checkgz', {
         params
     });
     return data;
 }
 
 async function addAttention(params) {
-    let data  = await gowhere.get('/content/addgz', {
+    let data = await gowhere.get('/content/addgz', {
         params
     });
     return data;
 }
 
 async function special(params) {
-    let data  = await gowhere.get('/page', {
+    let data = await gowhere.get('/page', {
         params
     });
     return data;
 }
 async function weidan(params) {
-    let data  = await gowhere.get('/page/pagef', {
+    let data = await gowhere.get('/page/pagef', {
         params
     });
     return data;
 }
 async function dijia(params) {
-    let data  = await gowhere.get('/page/pages', {
+    let data = await gowhere.get('/page/pages', {
         params
     });
     return data;
 }
 async function ziyouxing(params) {
-    let data  = await gowhere.get('/page/paget', {
+    let data = await gowhere.get('/page/paget', {
         params
     });
     return data;
 }
 
 
->>>>>>> 3cec31e772c2e19eda985df87c0ea1e11a7ba78e
 export default {
+    get,
     list,
     checkAttention,
     addAttention,
