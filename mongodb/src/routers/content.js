19f59faf580838f9c7e7073268c64be80fa6a81e
sunliuhaihua <1916218949@qq.com>
2019-11-01 09:13:59 +0800

    关注接口

--- mongodb/src/routers/content.js
+++ mongodb/src/routers/content.js
@@ -9,11 +9,17 @@ const colName = 'content';
 Router.get('/addgz', async (req, res) => {
     let { username, name } = req.query;
 
-    let info = await mongo.find(colName, { name });//查找关注的所有帖子
+    let result = await mongo.find('userinf', { name });//查找点击关注的name是否已存在
 
-    let result = await mongo.create('userinf',[{username,info}]);//保存帖子和用户
-    
-    res.send(result);
+    if (result.length) {
+        res.send(formatData({ code: 0 }))// 存在返回0
+    } else {
+        let info = await mongo.find(colName, { name });//查找关注的所有帖子
+        await mongo.create('userinf',[{username,info}]);//保存帖子和用户
+        res.send(formatData());//关注成功返回1；
+    }
+
+
 })
 
 
